ca4c7e5d152efe8d34f263d95628ad8acc0d9bd0
Olivia Marolf <87673860+livmarolf@users.noreply.github.com>
2023-10-31 09:29:44 -0600

    fixed landing bottom shapes styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Navbar from "./layouts/Navbar";
-import DesignProcess from "./pages/DesignProcess";
-// import Landing from "./pages/Landing";
+// import DesignProcess from "./pages/DesignProcess";
+import Landing from "./pages/Landing";
 import "./styles/app.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Landing /> */}
-      <DesignProcess />
+      <Landing />
+      {/* <DesignProcess /> */}
     </div>
   );
 }
