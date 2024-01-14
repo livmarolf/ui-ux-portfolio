e93a2451141cb49f73e397ecbe3fb97450a0f5f6
Olivia Marolf <87673860+livmarolf@users.noreply.github.com>
2024-01-14 13:37:17 -0700

    added contents page layout

---
+++ src/pages/Contents.js
@@ -0,0 +1,57 @@
+export default function Contents() {
+  return (
+    <div>
+      <div>
+        <div className="header-row">
+          <h1>CONTENTS</h1>
+        </div>
+
+        <div className="body-content">
+          <div>
+            <div>
+              <h2>DESIGN PROCESS</h2>
+              <p>01</p>
+            </div>
+            <p>
+              The outline and in-depth walkthrough of my design process from
+              start to finish.
+            </p>
+          </div>
+
+          <div>
+            <div>
+              <h2>CURRICULUM DESIGN</h2>
+              <p>02</p>
+            </div>
+            <p>
+              Creating a curriculum through practical design and planning for
+              effective learning outcomes.
+            </p>
+          </div>
+
+          <div>
+            <div>
+              <h2>HUDDLEUP</h2>
+              <p>03</p>
+            </div>
+            <p>
+              An application designed to enhance sports team management and
+              promote greater involvement.
+            </p>
+          </div>
+
+          <div>
+            <div>
+              <h2>REAL ESTATE</h2>
+              <p>04</p>
+            </div>
+            <p>
+              A tailored adaptive website design created for a real estate
+              professional.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
