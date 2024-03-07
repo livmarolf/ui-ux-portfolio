da2f4d9f592d20fda2a90477097d60d2f88255f9
Olivia Marolf <87673860+livmarolf@users.noreply.github.com>
2024-03-06 22:49:26 -0700

    more styling updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import emailjs from "@emailjs/browser";
 import { useRef, useState } from "react";
-import { ReactComponent as File } from "../assets/icons/file.svg";
+import { ReactComponent as File } from "../assets/icons/document-text.svg";
 import { ReactComponent as Phone } from "../assets/icons/phone.svg";
 import { ReactComponent as Envelope } from "../assets/icons/envelope.svg";
 import { ReactComponent as Linkedin } from "../assets/icons/linkedin.svg";
@@ -48,85 +48,121 @@ export default function Contact() {
 
   return (
     <div className="contact-content">
-      <div className="header-row">
-        <div className="title-container">
-          <div className="title">
-            <h1>CONTACT</h1>
+      <div className="left-column">
+        <div className="title">
+          <h1>CONTACT</h1>
+        </div>
+
+        <p className="sub-text">
+          Fill out the contact form, or contact me via one of the provided
+          methods.
+        </p>
+
+        <div className="contact-methods">
+          <div className="contact-method">
+            <div className="circle">
+              <File />
+            </div>
+
+            <div className="method-txt">
+              <a
+                target="blank"
+                href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:3a2fadb8-ab94-48b9-aa6f-1b8669e3a0af"
+              >
+                download resume
+              </a>
+            </div>
+          </div>
+
+          <div className="contact-method">
+            <div className="circle">
+              <Envelope />
+            </div>
+
+            <div className="method-txt">
+              <a href="mailto:livmarolf@gmail.com">livmarolf@gmail.com</a>
+            </div>
+          </div>
+
+          <div className="contact-method">
+            <div className="circle">
+              <Phone />
+            </div>
+
+            <div className="method-txt">
+              <a href="tel:+18018858122">(801) 885-8122</a>
+            </div>
+          </div>
+
+          <div className="contact-method">
+            <div className="circle">
+              <Linkedin />
+            </div>
+
+            <div className="method-txt">
+              <a
+                target="blank_"
+                href="https://www.linkedin.com/in/oliviamarolf/"
+              >
+                /in/oliviamarolf
+              </a>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="contact-container">
-        <div className="left-column">
-          <p>
-            Fill out the contact form, or contact me via one of the provided
-            methods.
-          </p>
-
-          <ul>
-            <li>
-              <File /> Download Resume
-            </li>
-            <li>
-              <Envelope /> livmarolf@gmail.com
-            </li>
-            <li>
-              <Phone /> (801) 885-8122
-            </li>
-            <li>
-              <Linkedin /> /in/oliviamarolf
-            </li>
-          </ul>
+      <div className="right-column">
+        <div className="dark-title">
+          <h1>let's talk</h1>
         </div>
 
-        <div className="right-column">
-          <form ref={form} onSubmit={handleSubmit}>
-            <div className="top-inputs">
-              <div className="input">
-                <label>Name</label>
-                <input
-                  maxLength="50"
-                  type="text"
-                  placeholder="John Doe"
-                  name="user_name"
-                  required
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-
-              <div className="input">
-                <label>Email</label>
-                <input
-                  required
-                  type="text"
-                  placeholder="example@yourdomain.com"
-                  name="user_email"
-                  onChange={(e) => handleChange(e)}
-                />
-                {invalidEmail && <p className="err-msg">Invalid Email</p>}
-              </div>
+        <form ref={form} onSubmit={handleSubmit}>
+          <div className="top-inputs">
+            <div className="input">
+              <label>Name</label>
+              <input
+                maxLength="30"
+                type="text"
+                placeholder="John Doe"
+                name="user_name"
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
 
             <div className="input">
-              <label>Message</label>
+              <label>Email</label>
               <input
+                maxLength="30"
                 required
                 type="text"
-                placeholder="Hi there..."
-                name="message"
-                maxLength="400"
-                onChange={(e) => setMessage(e.target.value)}
+                placeholder="example@yourdomain.com"
+                name="user_email"
+                onChange={(e) => handleChange(e)}
               />
-              <p className="message-len">{message.length}/400</p>
+              {invalidEmail && <p className="err-msg">Invalid Email</p>}
             </div>
+          </div>
 
-            <div className="btn-container">
-              <button disabled={invalidEmail || !name || !email || !message}>
-                Send
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className="input">
+            <label>Message</label>
+            <input
+              required
+              type="text"
+              placeholder="Hi there..."
+              name="message"
+              maxLength="400"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <p className="message-len">{message.length}/400</p>
+          </div>
+
+          <div className="btn-container">
+            <button disabled={invalidEmail || !name || !email || !message}>
+              send
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
