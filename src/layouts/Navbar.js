3701afdac8ad247e5cd94045c4a715b6be43e44d
Olivia Marolf <87673860+livmarolf@users.noreply.github.com>
2024-05-02 23:15:40 -0600

    media queries done

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -4,128 +4,132 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-left">
-        <div>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/livmarolf"
-          >
-            GITHUB
-          </a>
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21.8783 17.931L21.1702 9.83765L13.0769 10.5457"
-              stroke="white"
-              stroke-width="0.8"
-              stroke-miterlimit="10"
-              stroke-linecap="square"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M10.8319 22.1626L21.0703 9.96094"
-              stroke="white"
-              stroke-width="0.8"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+        <div className="links-container">
+          <div className="link">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/livmarolf"
+            >
+              GITHUB
+            </a>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21.8783 17.931L21.1702 9.83765L13.0769 10.5457"
+                stroke="white"
+                stroke-width="0.8"
+                stroke-miterlimit="10"
+                stroke-linecap="square"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M10.8319 22.1626L21.0703 9.96094"
+                stroke="white"
+                stroke-width="0.8"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+
+          <div className="link">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/oliviamarolf/"
+            >
+              LINKEDIN
+            </a>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21.8783 17.931L21.1702 9.83765L13.0769 10.5457"
+                stroke="white"
+                stroke-width="0.8"
+                stroke-miterlimit="10"
+                stroke-linecap="square"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M10.8319 22.1626L21.0703 9.96094"
+                stroke="white"
+                stroke-width="0.8"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
         </div>
 
-        <div>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/oliviamarolf/"
-          >
-            LINKEDIN
-          </a>
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21.8783 17.931L21.1702 9.83765L13.0769 10.5457"
-              stroke="white"
-              stroke-width="0.8"
-              stroke-miterlimit="10"
-              stroke-linecap="square"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M10.8319 22.1626L21.0703 9.96094"
-              stroke="white"
-              stroke-width="0.8"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </div>
+        <div className="links-container">
+          <div className="link">
+            <Link>RESUME</Link>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21.8783 17.931L21.1702 9.83765L13.0769 10.5457"
+                stroke="white"
+                stroke-width="0.8"
+                stroke-miterlimit="10"
+                stroke-linecap="square"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M10.8319 22.1626L21.0703 9.96094"
+                stroke="white"
+                stroke-width="0.8"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
 
-        <div>
-          <Link>RESUME</Link>
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21.8783 17.931L21.1702 9.83765L13.0769 10.5457"
-              stroke="white"
-              stroke-width="0.8"
-              stroke-miterlimit="10"
-              stroke-linecap="square"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M10.8319 22.1626L21.0703 9.96094"
-              stroke="white"
-              stroke-width="0.8"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </div>
-
-        <div>
-          <Link>EMAIL</Link>
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21.8783 17.931L21.1702 9.83765L13.0769 10.5457"
-              stroke="white"
-              stroke-width="0.8"
-              stroke-miterlimit="10"
-              stroke-linecap="square"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M10.8319 22.1626L21.0703 9.96094"
-              stroke="white"
-              stroke-width="0.8"
-              stroke-miterlimit="10"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+          <div className="link">
+            <Link>EMAIL</Link>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21.8783 17.931L21.1702 9.83765L13.0769 10.5457"
+                stroke="white"
+                stroke-width="0.8"
+                stroke-miterlimit="10"
+                stroke-linecap="square"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M10.8319 22.1626L21.0703 9.96094"
+                stroke="white"
+                stroke-width="0.8"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
         </div>
       </div>
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,6 @@ import Navbar from "../layouts/Navbar";
 import ParallaxText from "../components/motion/ParallaxText";
 import ProgressCircle from "../components/motion/ProgressCircle";
 import Paragraph from "../components/motion/Paragraph";
-import SlideCard from "../components/motion/SlideCard";
 import { pageData } from "../data/CaseStudies";
 import AnimatedCharacters from "../components/motion/AnimatedCharacters";
 import CaseCard from "../components/CaseCard";
@@ -18,80 +17,87 @@ With over four years of experience in software development and UI/UX design, I b
       <Navbar />
 
       <div className="title-container">
-        <div className="left">
-          <div className="name-block">
-            <AnimatedCharacters text={"OLIVIA"} delayFactor={0} />
+        <div className="first-last">
+          <div>
+            <div className="name-block">
+              <AnimatedCharacters text={"OLIVIA"} delayFactor={0} />
 
-            <svg
-              width="21"
-              height="21"
-              viewBox="0 0 21 21"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="10.5"
-                cy="10.5"
-                r="9.5"
-                stroke="#7E7E7E"
-                strokeWidth="2"
-              />
-              <path
-                d="M12.34 4V17.2H10.12V6.52L7.02 8.52L6 6.86L10.3 4H12.34Z"
-                fill="#7E7E7E"
-              />
-            </svg>
+              <svg
+                width="21"
+                height="21"
+                viewBox="0 0 21 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="10.5"
+                  cy="10.5"
+                  r="9.5"
+                  stroke="#7E7E7E"
+                  strokeWidth="2"
+                />
+                <path
+                  d="M12.34 4V17.2H10.12V6.52L7.02 8.52L6 6.86L10.3 4H12.34Z"
+                  fill="#7E7E7E"
+                />
+              </svg>
+            </div>
           </div>
 
-          <div className="name-block">
-            <AnimatedCharacters text={"UI/UX DESIGNER"} delayFactor={0.96} />
+          <div>
+            <div className="name-block">
+              <AnimatedCharacters text={"MAROLF"} delayFactor={0.48} />
 
-            <svg
-              width="21"
-              height="21"
-              viewBox="0 0 21 21"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="10.5"
-                cy="10.5"
-                r="9.5"
-                stroke="#7E7E7E"
-                strokeWidth="2"
-              />
-              <path
-                d="M7.12844 4.56305C8.26605 4.18768 9.51376 4 10.8716 4C12.2294 4 13.2141 4.2522 13.8257 4.7566C14.4495 5.24927 14.7615 6.03519 14.7615 7.11437C14.7615 7.58358 14.6636 8.00586 14.4679 8.38123C14.2844 8.7566 14.0948 9.03226 13.8991 9.20821C13.7034 9.37243 13.4587 9.54839 13.1651 9.73607C13.8135 10.0293 14.2783 10.3519 14.5596 10.7038C14.8532 11.044 15 11.6012 15 12.3754C15 13.607 14.6758 14.522 14.0275 15.1202C13.3914 15.7067 12.37 16 10.9633 16C10.4128 16 9.80734 15.9531 9.14679 15.8592C8.48624 15.7771 7.96024 15.6891 7.56881 15.5953L7 15.4545L7.12844 13.9413C8.47401 14.176 9.66055 14.2933 10.6881 14.2933C12.156 14.2815 12.8899 13.6598 12.8899 12.4282C12.8899 11.8651 12.7003 11.437 12.3211 11.1437C11.9419 10.8504 11.4404 10.6979 10.8165 10.6862H8.37615V9.04985H10.8165C11.2691 9.04985 11.685 8.8739 12.0642 8.52199C12.4557 8.15836 12.6514 7.72434 12.6514 7.21994C12.6514 6.70381 12.4801 6.32258 12.1376 6.07625C11.8073 5.82991 11.3486 5.70674 10.7615 5.70674C9.79511 5.70674 8.79816 5.78886 7.77064 5.95308L7.29358 6.04106L7.12844 4.56305Z"
-                fill="#7E7E7E"
-              />
-            </svg>
-            <p>© OLIVIA MAROLF</p>
+              <svg
+                width="21"
+                height="21"
+                viewBox="0 0 21 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="10.5"
+                  cy="10.5"
+                  r="9.5"
+                  stroke="#7E7E7E"
+                  strokeWidth="2"
+                />
+                <path
+                  d="M15 16H7V14.301L10.0682 11.2966C10.9969 10.3905 11.6494 9.68107 12.0259 9.1684C12.4024 8.65574 12.5906 8.09538 12.5906 7.48733C12.5906 6.86736 12.4086 6.42623 12.0447 6.16393C11.6808 5.90164 11.0847 5.77049 10.2565 5.77049C9.47843 5.77049 8.61882 5.85395 7.67765 6.02086L7.20706 6.0924L7.07529 4.51863C8.29255 4.17288 9.55373 4 10.8588 4C13.4565 4 14.7553 5.09687 14.7553 7.29061C14.7553 8.14903 14.5545 8.89419 14.1529 9.52608C13.7639 10.158 13.0612 10.921 12.0447 11.8152L9.39059 14.2653H15V16Z"
+                  fill="#7E7E7E"
+                />
+              </svg>
+            </div>
           </div>
         </div>
 
-        <div className="right">
+        <div className="profession">
           <div className="name-block">
-            <AnimatedCharacters text={"MAROLF"} delayFactor={0.48} />
+            <AnimatedCharacters text={"UI/UX"} delayFactor={0.96} />
+            <div className="couple">
+              <AnimatedCharacters text={"DESIGNER"} delayFactor={1.44} />
 
-            <svg
-              width="21"
-              height="21"
-              viewBox="0 0 21 21"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="10.5"
-                cy="10.5"
-                r="9.5"
-                stroke="#7E7E7E"
-                strokeWidth="2"
-              />
-              <path
-                d="M15 16H7V14.301L10.0682 11.2966C10.9969 10.3905 11.6494 9.68107 12.0259 9.1684C12.4024 8.65574 12.5906 8.09538 12.5906 7.48733C12.5906 6.86736 12.4086 6.42623 12.0447 6.16393C11.6808 5.90164 11.0847 5.77049 10.2565 5.77049C9.47843 5.77049 8.61882 5.85395 7.67765 6.02086L7.20706 6.0924L7.07529 4.51863C8.29255 4.17288 9.55373 4 10.8588 4C13.4565 4 14.7553 5.09687 14.7553 7.29061C14.7553 8.14903 14.5545 8.89419 14.1529 9.52608C13.7639 10.158 13.0612 10.921 12.0447 11.8152L9.39059 14.2653H15V16Z"
-                fill="#7E7E7E"
-              />
-            </svg>
+              <svg
+                width="21"
+                height="21"
+                viewBox="0 0 21 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="10.5"
+                  cy="10.5"
+                  r="9.5"
+                  stroke="#7E7E7E"
+                  strokeWidth="2"
+                />
+                <path
+                  d="M7.12844 4.56305C8.26605 4.18768 9.51376 4 10.8716 4C12.2294 4 13.2141 4.2522 13.8257 4.7566C14.4495 5.24927 14.7615 6.03519 14.7615 7.11437C14.7615 7.58358 14.6636 8.00586 14.4679 8.38123C14.2844 8.7566 14.0948 9.03226 13.8991 9.20821C13.7034 9.37243 13.4587 9.54839 13.1651 9.73607C13.8135 10.0293 14.2783 10.3519 14.5596 10.7038C14.8532 11.044 15 11.6012 15 12.3754C15 13.607 14.6758 14.522 14.0275 15.1202C13.3914 15.7067 12.37 16 10.9633 16C10.4128 16 9.80734 15.9531 9.14679 15.8592C8.48624 15.7771 7.96024 15.6891 7.56881 15.5953L7 15.4545L7.12844 13.9413C8.47401 14.176 9.66055 14.2933 10.6881 14.2933C12.156 14.2815 12.8899 13.6598 12.8899 12.4282C12.8899 11.8651 12.7003 11.437 12.3211 11.1437C11.9419 10.8504 11.4404 10.6979 10.8165 10.6862H8.37615V9.04985H10.8165C11.2691 9.04985 11.685 8.8739 12.0642 8.52199C12.4557 8.15836 12.6514 7.72434 12.6514 7.21994C12.6514 6.70381 12.4801 6.32258 12.1376 6.07625C11.8073 5.82991 11.3486 5.70674 10.7615 5.70674C9.79511 5.70674 8.79816 5.78886 7.77064 5.95308L7.29358 6.04106L7.12844 4.56305Z"
+                  fill="#7E7E7E"
+                />
+              </svg>
+            </div>
+            <p>© OLIVIA MAROLF</p>
           </div>
         </div>
       </div>
